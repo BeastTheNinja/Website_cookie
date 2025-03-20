21c50c1db898b71b9864f9a6c7a01b432c770ddf
BeastTheNinja <134609528+BeastTheNinja@users.noreply.github.com>
2025-03-20 09:04:53 +0100

    Update #1

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,1 +1,14 @@
 // MAIN CODING
+
+// Gallery
+const thumbs = document.querySelectorAll("#thumbs img");
+const bigImage = document.querySelector("#big");
+
+thumbs.forEach((thumb) => {
+    thumb.addEventListener("click", function (e) {
+        let tempSrc = bigImage.src;
+        bigImage.src = e.target.src;
+        e.target.src = tempSrc;
+    });
+});
+//
