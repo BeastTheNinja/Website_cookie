c7ddf1ffcaeb70d0ee5f8ea8b7fdddd58b9f167e
BeastTheNinja <134609528+BeastTheNinja@users.noreply.github.com>
2025-03-20 09:09:14 +0100

    Navbar done #1 holds updated

--- assests/js/navbar.js
+++ assests/js/navbar.js
@@ -1,1 +1,14 @@
 // NAVIGATION BAR SCRIPT
+const menuToggle = document.querySelector(".menu-toggle");
+const navMenu = document.querySelector(".nav-list");
+
+menuToggle.addEventListener("click", ()=>{
+    navMenu.classList.toggle("active");
+});
+
+
+// Ændret, så den bruger `navMenu` i stedet for at oprette en ny variabel
+window.addEventListener("scroll", (e)=>{
+    // console.log(e);
+    navMenu.style.backgroundColor = window.scrollY > 50 ? "#000" : "#777777";
+});
